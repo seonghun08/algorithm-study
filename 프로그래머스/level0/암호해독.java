02f5b058ca746673a68e79c04f49fd4f22fae4a1
seonghun <68460507+seonghun08@users.noreply.github.com>
2022-12-08 18:16:51 +0900

    commit

---
+++ 프로그래머스/level0/암호해독.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class 암호해독 {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        for (int i = code-1; i < cipher.length(); i+=code) {
+            answer += cipher.charAt(i);
+        }
+        return answer;
+    }
+}
