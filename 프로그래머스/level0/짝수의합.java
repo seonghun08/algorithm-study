28b3797dced9eb6fcb3f5edc300abf03e61e3bcb
seonghun <68460507+seonghun08@users.noreply.github.com>
2022-12-13 20:40:54 +0900

    20221213

---
+++ 프로그래머스/level0/짝수의합.java
@@ -0,0 +1,14 @@
+package level0;
+
+// 120831
+public class 짝수의합 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
