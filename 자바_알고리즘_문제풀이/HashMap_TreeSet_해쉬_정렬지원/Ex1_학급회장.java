56f2e91ff640bedc6c897db0b0c97f4ee5e46ba9
shajh9708 <shajh9708@naver.com>
2023-01-24 01:10:44 +0900

    20230124 HashMap 사용법

---
+++ 자바_알고리즘_문제풀이/HashMap_TreeSet_해쉬_정렬지원/Ex1_학급회장.java
@@ -0,0 +1,32 @@
+package HashMap_TreeSet_해쉬_정렬지원;
+
+import java.util.*;
+
+public class Ex1_학급회장 {
+    public static char solution(int n, String s) {
+        char answer = ' ';
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (char key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = in.next();
+
+        System.out.println(solution(n, s));
+    }
+}
--- 프로그래머스/Main.java
+++ 프로그래머스/Main.java
@@ -1,19 +1,8 @@
 import java.util.*;
 
 public class Main {
-    public static int solution(int n, int k, int[] arr) {
-        int answer = 0, lt = 0;
-        int cnt = 0;
-        for (int rt = 0; rt < n; rt++) {
-            if (arr[rt] == 0) cnt++;
-            while (cnt > k) {
-                int result = arr[lt++];
-                if (result == 0) {
-                    cnt--;
-                }
-            }
-            answer = Math.max(answer, rt - lt + 1);
-        }
+    public static char solution(int n, String s) {
+        char answer = ' ';
 
         return answer;
     }
@@ -21,13 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int k = in.nextInt();
+        String s = in.next();
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-
-        System.out.println(solution(n, k, arr));
+        System.out.println(solution(n, s));
     }
 }
