ced13f6ea71e9f8b0f9071620394d67e35d31e5b
shajh9708 <shajh9708@naver.com>
2023-02-21 22:55:07 +0900

    20230221 hash, sliding window

---
+++ 자바_알고리즘_문제풀이/HashMap_TreeSet_해쉬_정렬지원/Ex3_매출액의종류.java
@@ -0,0 +1,46 @@
+package HashMap_TreeSet_해쉬_정렬지원;
+
+import java.util.*;
+
+public class Ex3_매출액의종류 {
+    public static ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int lt = 0;
+        int rt = k - 1;
+
+        for (int i = lt; i < rt; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        while (rt < n) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            answer.add(map.size());
+
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
+
+            lt++;
+            rt++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.next());
+        int k = Integer.parseInt(in.next());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(in.next());
+        }
+
+        ArrayList<Integer> answer = solution(n, k, arr);
+        for (int num : answer) {
+            System.out.println(num + " ");
+        }
+    }
+}
--- 프로그래머스/Main.java
+++ 프로그래머스/Main.java
@@ -4,15 +4,14 @@ public class Main {
     public static String solution(String s1, String s2) {
         String answer = "YES";
 
-
         return answer;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s1 = in.next();
-        String s2 = in.next();
 
-        System.out.println(solution(s1, s2));
+        int n = Integer.parseInt(in.next());
+        int k = Integer.parseInt(in.next());
+
     }
 }
