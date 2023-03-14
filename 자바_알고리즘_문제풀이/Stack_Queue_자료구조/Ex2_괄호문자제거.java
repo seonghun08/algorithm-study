9cc4a212df0303f2154c39569acf6f23bb51c087
shajh9708 <shajh9708@naver.com>
2023-03-14 23:56:41 +0900

    Stack 자료구조 활용

---
+++ 자바_알고리즘_문제풀이/Stack_Queue_자료구조/Ex2_괄호문자제거.java
@@ -0,0 +1,43 @@
+package Stack_Queue_자료구조;
+
+import java.util.*;
+
+public class Ex2_괄호문자제거 {
+    public static String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') st.push(ch);
+            else if (ch == ')') st.pop();
+            else if (st.isEmpty()) answer.append(ch);
+        }
+        return String.valueOf(answer);
+    }
+
+    public static String solution2(String str) {
+        StringBuffer sb = new StringBuffer();
+        Stack<Character> st = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == ')') {
+                while (st.pop() != '(');
+            } else {
+                st.push(ch);
+            }
+        }
+        for (Character ch : st) {
+            sb.append(ch);
+        }
+        return String.valueOf(sb);
+    }
+
+    /**
+     * (A(BC)D)EF(G(H)(IJ)K)LM(N)
+     */
+    public static void Ex2_괄호문자제거(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        System.out.println(solution(str));
+        System.out.println(solution2(str));
+    }
+}
--- 프로그래머스/Main.java
+++ 프로그래머스/Main.java
@@ -1,16 +1,39 @@
 import java.util.*;
 
 public class Main {
-    public static String solution(String str) {
-        String answer = "";
+    public static int solution(String str) {
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
 
+        /**
+         * 3573*+5-323*++
+         */
+        for (char ch : str.toCharArray()) {
+            if (isInteger(ch)) {
+                st.push(Integer.parseInt(String.valueOf(ch)));
+            } else {
+                if (answer == 0) answer = st.pop();
+                if (ch == '+') answer += st.pop();
+                if (ch == '-') answer -= st.pop();
+                if (ch == '*') answer *= st.pop();
+                if (ch == '/') answer /= st.pop();
+            }
+        }
         return answer;
+    }
+
+    public static boolean isInteger(Character c) {
+        try {
+            Integer.parseInt(String.valueOf(c));
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.next();
-
         System.out.println(solution(str));
     }
 }
