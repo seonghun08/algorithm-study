79666d935d2f1cef9e4cf480f4230d9bdada732a
shajh9708 <shajh9708@naver.com>
2023-08-02 23:38:24 +0900

    BFS 복습 및 재귀 함수 이해

--- 자바_알고리즘_문제풀이/TestCode.java
+++ 자바_알고리즘_문제풀이/TestCode.java
@@ -1,8 +1,38 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 public class TestCode {
-    public static void main(String[] args) throws Exception {
 
+    int[] dis = {1, -1, 5};
+    int[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+    private int solution(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;
+        Q.offer(s);
+        int level = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i <len; i++) {
+                int x = Q.poll();
+                for (int m : dis) {
+                    int nx = x + m;
+                    if (nx == e) return ++level;
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        ch[nx] = 1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        TestCode main = new TestCode();
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int e = in.nextInt();
+        System.out.println(main.solution(s, e));
     }
 }
